d28bad8eb04850d1385ec62721d20455bc912f83
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-03-26 16:42:59 +0100

    feat: first structure on html file and add value extraction on js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-alert("ciao");
+//prelevo elemento del button send
+const sendBtn = document.getElementById("send");
+console.log(sendBtn);
+
+//aggiungo evento in ascolto al click
+sendBtn.addEventListener("click", function () {
+    //prelevo valore dall'input dell'username
+    const userName = document.getElementById("username").value; // string | null
+    console.log("Username:", userName);
+
+    //prelevo valore dall'input dell'username
+    let kmUser = document.getElementById("user-km").value.replace(/,/g, "."); // string | null
+    console.log("KmUser:", kmUser);
+
+    //prelevo valore dall'input dell'età
+    let ageUser = document.getElementById("user-age").value; // string | null
+    console.log("Età:", ageUser, typeof ageUser);
+})
